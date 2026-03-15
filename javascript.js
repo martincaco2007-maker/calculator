0d5bd775cfc84c4f316398689f95aa2cc18c9b33
martin <m.caco@student.utwente.nl>
2026-03-16 00:09:46 +0100

    finished logic for calculator

--- javascript.js
+++ javascript.js
@@ -1,45 +1,102 @@
 let number1 = null;
 let number2 = null;
+let current = number1;
 let operator = null;
+let showingResult = false;
+let result = document.querySelector(".result");
+
 
 function operate(number1, number2, operator) {
     if (operator == "+") {
-        return number1 + number2;
+        result.textContent = Number(number1) + Number(number2);
+        return Number(number1) + Number(number2);
     }
     else if (operator == "-") {
-        return number1 - number2;
+        result.textContent = Number(number1) - Number(number2);
+        return Number(number1) - Number(number2);
     }
-    else if (operator == "/") {
-        if (number2 === 0) return "Undefined";
-        return number1 / number2;
+    else if (operator == "÷") {
+        if (number2 === "0") return "Undefined";
+        result.textContent = Number(number1) / Number(number2);
+        return Number(number1) / Number(number2);
     }
-    else if (operator == "*") {
-        return number1 * number2;
+    else if (operator == "×") {
+        result.textContent = Number(number1) * Number(number2);
+        return Number(number1) * Number(number2);
     }
+
 }
 
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        switch(button.textContent) {
-            case "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9":
-                if (number1 == null) number1 = Number(button.textContent);
-                else number2 = Number(button.textContent);
-                break;
-            case "+" || "-" || "/" || "*":
-                if (number1 != null && number2 != null) {
-                    number1 = operate(number1, number2, button.textContent);
-                    number2 = null;
-                }
-                operator = button.textContent;
-                break;
-            case "=":
-                operate(number1, number2, operator)
+
+        if (button.textContent >= "0" && button.textContent <= "9") {
+
+            if (showingResult) {
+                current = null;
+                showingResult = false;
+            }
+
+            if (current == null) {
+                current = button.textContent;
+                result.textContent = current;
+            }
+            else {
+                current = current.concat(button.textContent);
+                result.textContent = current;
+            }
         }
-    })
+        else if (
+            button.textContent === "+" ||
+            button.textContent === "-" ||
+            button.textContent === "÷" ||
+            button.textContent === "×"
+        ) {
+            if (number1 === null) {
+                number1 = current;
+                operator = button.textContent;
+                current = null;
+            }
+
+            else if (current !== null) {
+                number2 = current;
+
+                number1 = operate(number1, number2, operator);
+                result.textContent = number1;
+                operator = button.textContent;
+                current = null;
+                number2 = null;
+            }
+        }
+
+        else if (button.textContent === "=") {
+            number2 = current;
+            number1 = operate(number1, number2, operator);
+            current = String(number1);
+            number2 = null;
+            result.textContent = number1;
+            showingResult = true;
+
+        }
+
+        else if (button.textContent === "Clear") {
+            number1 = null;
+            number2 = null;
+            current = null;
+            operator = null;
+            result.textContent = "";
+            showingResult = false;
+        }
+
+        else if (button.textContent === "⌫") {
+            current = current.slice(0, -1);
+            result.textContent = current;
+        }
+    }
+    )
 })
 
-// used to display result in .result
-function displayResult()
+
 
